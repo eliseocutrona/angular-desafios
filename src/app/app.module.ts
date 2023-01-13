f00c7c91a64e00c8c65270af5d367971c4e3b8a8
eliseocutrona <eliseocutrona@gmail.com>
2023-01-12 21:27:21 -0300

    componentes prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { StudentsComponent } from './students/students.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+   StudentsComponent,
+    // ToolbarComponent
   ],
   imports: [
     BrowserModule,
